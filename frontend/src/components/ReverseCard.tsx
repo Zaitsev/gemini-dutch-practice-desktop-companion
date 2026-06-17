09098be77fffed2061f06f1814a605134688a72e
vladzaitsev <vladzaitsev@gmail.com>
2026-06-17 20:59:29 +0200

    refactor: clean up imports in ReverseCard and WordDeckAssignments components

--- frontend/src/components/ReverseCard.tsx
+++ frontend/src/components/ReverseCard.tsx
@@ -1,8 +1,8 @@
 import { Eye } from "lucide-react";
 import { useAppStateContext, type Word } from "../provider";
-import { PlayAudioUrl } from "./PlayAudioUrl";
 import { Examples } from "./Examples";
-import { WordDeckAssignments, WordDeckBadges } from "./WordDeckAssignments";
+import { PlayAudioUrl } from "./PlayAudioUrl";
+import { WordDeckBadges } from "./WordDeckAssignments";
 /** ReverseCard component displays a single word card English->Dutch */
 export const ReverseCard: React.FC<{ activeCard: Word }> = ({ activeCard }) => {
     const { isFlipped } = useAppStateContext();
